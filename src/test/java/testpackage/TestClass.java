646599582274d827a0a1d67533780b375c83d2b2
rohitk20030825 <rohitk20030825@gmail.com>
2020-12-29 20:15:35 +0530

    Commit

--- src/test/java/testpackage/TestClass.java
+++ src/test/java/testpackage/TestClass.java
@@ -6,21 +6,19 @@ import org.testng.annotations.Test;
 
 public class TestClass {
 
-	
 	@BeforeTest
 	public void initiate() {
 		System.out.println("This is going to initiate the project");
 	}
-	
+
 	@Test
-	public void testMethod1()
-	{
+	public void testMethod1() {
 		System.out.println("Hello World Program");
 	}
-	
+
 	@AfterTest
 	public void cleanup() {
 		System.out.println("This is going to clean up the residue");
 	}
-	
+
 }
